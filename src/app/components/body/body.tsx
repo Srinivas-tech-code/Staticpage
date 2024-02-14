24654c2f18369c9e17a5c82a106f2e06611bbd5e
Srinivas <srinivaschiyyedu7816@gmail.com>
2024-02-14 15:38:08 +0530

    first commit

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
+
 import styles from './body.module.css'
 
 interface propshere{
@@ -9,11 +10,11 @@ interface propshere{
 
 const Body:React.FC<propshere> = () => {
   return (
-    <body>
+    
     <div className={styles.container}>
-          <Image className={styles.Image} alt={'Sun is rising'} width={414} height={425.6} src={'/ms-dhoni.jpg'}/>
+          <Image sizes="(max-width:480) and (min-width:320) 100vw,(max-width: 768px) 100vw, (max-width: 1280px) 100vw" width={1280} height={1280} src={'/dhoni2.png'} layout='responsive' alt={'Sun is rising'} />
     </div>
-    </body>
+    
    
 
   )
--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,30 +4,36 @@ import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.one}>
+    <div className={styles.container}>
     <div className={styles.two}>
       <div className={styles.three}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"  viewBox="0 0 16 16">
   <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1z"/>
 </svg>
         
-        <svg style={{flex:'left'}} fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>
-        <svg fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path></svg>
+        <svg  fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>
+        <Image src={'/share.png'} width={24} height={24} alt='share'/>
       </div>
+      <div className='rtu'>
       <button className={styles.four}>
   Lead +100
+
+<Image className='zero' alt='ucoin'  width={18} height={18} src={'/ucoin.png'}/>
 </button>
+</div>
     </div>
     <div className={styles.five}>50 interested</div>
     <div className={styles.six} >
     <Image width={50} height={50}  alt={"Dhoni"} src={"/Virat.jpg"}/>
     </div>
-    <div className="mr-4" style={{display:'inline-flex'}}> 
-        <span style={{ flex: '1 1 20%' }}  className="ml-2">2100</span>
-        <span style={{ flex: '1 1 20%' }}  className="">Gross Coins</span>
+    <div className={styles.seven}> 
+        <span  className={styles.abcd}>1100</span>
+        <br/>
+        <span   className={styles.cvbn}>Virat_kohli</span>
+        <span className={styles.klmn}> in Lead</span>
         </div>
-        <span style={{ flex: '1 1 20%' }} className=""><svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></span>
-        <span style={{ flex: '1 1 20%' }} className=""><svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} viewBox="0 0 128 512"><path fill="#B197FC" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg></span>     
+        <span  className="opt"><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></span>
+        
     </div>
 
   )
