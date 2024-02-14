bf933a125d79fa0d7d6043cb2160d1d52968e9b7
Srinivas <srinivaschiyyedu7816@gmail.com>
2024-02-14 15:50:47 +0530

    first commit

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
         <span className={styles.klmn}> in Lead</span>
         </div>
         <span  className="opt"><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></span>
-        
+        <div className='ppt'><Image src={'/time.png'} alt='time' width={73} height={33}/> </div>
     </div>
 
   )
