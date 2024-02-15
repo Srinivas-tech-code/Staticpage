49ca6daea3a519289bc0d49a5d6db3cf17919b56
Srinivas <srinivaschiyyedu7816@gmail.com>
2024-02-15 11:03:26 +0530

    first commit

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -13,27 +13,35 @@ const Footer = () => {
         
         <svg  fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>
         <Image src={'/share.png'} width={24} height={24} alt='share'/>
-      </div>
-      <div className='rtu'>
-      <button className={styles.four}>
+        <button className={styles.four}>
   Lead +100
 
 <Image className='zero' alt='ucoin'  width={18} height={18} src={'/ucoin.png'}/>
 </button>
-</div>
+
+
+      </div>
+      
+     
+
+      
     </div>
-    <div className={styles.five}>50 interested</div>
+    <div className={styles.box}>
+    <div className={styles.five}>50 interested</div>    
     <div className={styles.six} >
-    <Image width={50} height={50}  alt={"Dhoni"} src={"/Virat.jpg"}/>
+    <Image width={40} height={40}  alt={"Dhoni"} src={"/Virat.jpg"}/>
     </div>
+    <Image className={styles.gold} width={10} height={18} src={'/goldstack.png'} alt={'gold coins'}/>
     <div className={styles.seven}> 
-        <span  className={styles.abcd}>1100</span>
-        <br/>
-        <span   className={styles.cvbn}>Virat_kohli</span>
-        <span className={styles.klmn}> in Lead</span>
+        <div>1100</div>
+        <span style={{fontWeight:'bold'}} >Virat_kohli</span>
+        <span style={{color:'gray'}}> in Lead</span>
+        <div><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg>
+        <Image  src={'/time.png'} alt='time' width={73} height={33}/>
         </div>
-        <span  className="opt"><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></span>
-        <div className='ppt'><Image src={'/time.png'} alt='time' width={73} height={33}/> </div>
+        
+        </div>
+        </div>
     </div>
 
   )
