c1f878f5b070099b8a7eaf939c3cb9b1ddfc0fa2
Srinivas <srinivaschiyyedu7816@gmail.com>
2024-02-15 20:50:19 +0530

    first commit

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -41,11 +41,12 @@ const Footer = () => {
         
         </div>
         <div className={styles.before}><svg  xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></div>
-        <div style={{position:'absolute',marginTop:'9%'}} >@Virat_kohli...Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, velit.</div>
         <div className={styles.last} >
         <Image  src={'/time.png'} alt='time' width={73} height={33}/>
-        
         </div>
+        <br/>
+        <div className={styles.prev} >@Virat_kohli...Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, velit.</div>
+        
         
     </div>
     
