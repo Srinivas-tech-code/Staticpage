5a264cff138b9eff714d614d3d97cae89a938f4b
Srinivas <srinivaschiyyedu7816@gmail.com>
2024-02-16 21:00:21 +0530

    first commit

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,32 +4,28 @@ import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-    <div className={styles.two}>
+    <div className={styles.flexsupercontainer}>
+    <div className={styles.flexcontainer}>
       <div className={styles.three}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"  viewBox="0 0 16 16">
   <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1z"/>
 </svg>
-        
         <svg  fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path></svg>
         <Image src={'/share.png'} width={24} height={24} alt='share'/>
-        <button className={styles.four}>
+      </div>
+      <div className={styles.four}>
+      <button style={{backgroundColor:'rgb(82, 22, 138)',color:'white'}}>
   Lead +100
-
 <Image className='zero' alt='ucoin'  width={18} height={18} src={'/ucoin.png'}/>
 </button>
-
-
-      </div>
-      
-     
-
-      
-    </div>
+</div>
+</div>
     <div className={styles.box}>
-    <div className={styles.five}>50 interested</div>    
+    50 interested
+    </div>
     <div className={styles.six} >
-    <Image width={40} height={40}  alt={"Dhoni"} src={"/Virat.jpg"}/>
+      <div className={styles.sixleft}>
+    <Image style={{borderRadius:'90%',overflow:'hidden'}} width={40} height={40}  alt={"Dhoni"} src={"/Virat.jpg"}/>
     </div>
     <Image className={styles.gold} width={10} height={18} src={'/goldstack.png'} alt={'gold coins'}/>
     <div className={styles.seven}> 
@@ -39,10 +35,11 @@ const Footer = () => {
         
         </div>
         
-        </div>
+       
         <div className={styles.before}><svg  xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></div>
         <div className={styles.last} >
         <Image  src={'/time.png'} alt='time' width={73} height={33}/>
+        </div>
         </div>
         <br/>
         <div className={styles.prev} >@Virat_kohli...Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, velit.</div>
--- src/app/components/head/head.tsx
+++ src/app/components/head/head.tsx
@@ -6,29 +6,31 @@ import styles from './head.module.css'
 const Head:React.FC = () => {
   return (
 
-  <div className={styles.container}>
-      
+  <div className={styles.flexcontainer}>
+      <div className={styles.leftbox}>  
       <Image className={styles.Image} width={40} height={40}  alt={"Dhoni"} src={"/Dhoni.png"}/>
-      
-      <div className={styles.hhh}>
-        <div className={styles.one}>Dhoni</div>
-        <div className={styles.two}>6 June 2021, 12:10 pm</div>
-      
-      
-        <div className={styles.three}>
+      <div className={styles.person} >
+        <div>Dhoni</div>
+        <div>6 June 2021, 12:10pm</div>
+      </div>
+      </div>
+      <div className={styles.rightbox}>
+        <div className={styles.abc}>
             <Image width={20} height={30} src={'/goldstack.png'} alt={'gold coins'}/>
         </div>
-        <div className={styles.fivesix}>
-        <div>2100
+        <div className={styles.def}>
+           <div>2100</div>
+           <div>Gross Coins</div>
+          </div>   
+          <div className={styles.hij}>
+        <div><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></div>
+        <div><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 128 512"><path fill="#B197FC" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg></div>     
+        </div>
+        </div>
         
-        <div >Gross Coins</div>
-        </div>
-        </div>
-        <div className={styles.seven}><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 576 512"><path fill="#42aa1d" d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32H544c17.7 0 32 14.3 32 32V288c0 17.7-14.3 32-32 32s-32-14.3-32-32V205.3L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160H384z"/></svg></div>
-        <div className={styles.eight}><svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 128 512"><path fill="#B197FC" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg></div>     
       </div>
       
-    </div>
+  
     
   )
 }
